445fcb91ea9bd250a5f465eb32e5899bafa7fc12
Evsey <evseyru1@mail.ru>
2022-05-31 03:34:49 +0300

    Added ability to export converted documents to specific folder

--- API_README.md
+++ API_README.md
@@ -3,5 +3,6 @@
  - -p [PROCESSES], --processes [PROCESSES]: The number of processes to run in parallel for conversion. Defaults to the number of cores on the system.
  - -ct [CONVTYPE], --convtype [CONVTYPE]: The conversion method used, either msoffice or unoserver. Defaults to "msoffice".
  - -k, --kill: Kill the convert server after conversion.
+ - -o [OUTPUT-FOLDER], --output-folder [OUTPUT-FOLDER]: Directory where to store the converted documents.
  - infiles: List of files to convert, which must end in .xls or .docx. .xls conversion works only with msoffice conversion method.
 
--- unoserver_worker.py
+++ unoserver_worker.py
@@ -1,11 +1,19 @@
-import unoserver
 import multiprocessing
 from pathlib import Path
 import subprocess
 
 
-def convert_to_pdf(file_path: Path, server_port):
+def convert_to_pdf(file_path: Path, base_server_port, out_path: Path = None):
+	"""
+	:param file_path: Path to the document to be converted
+	:param base_server_port: Server port of the first unoserver
+	:param out_path: Directory where to store the converted document
+	"""
 	worker_id = multiprocessing.current_process()._identity[0] - 1
 	infile = str(file_path.absolute())
-	outfile = str(file_path.absolute().parent) + "/" + file_path.absolute().stem + ".pdf"
-	subprocess.Popen(["unoconvert", "--port", str(server_port + worker_id), infile, outfile], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	if out_path is not None:
+		outfile = str(out_path.absolute()) + ("/" if str(out_path.absolute())[-1] != "/" else "") + file_path.absolute().stem + ".pdf"
+	else:
+		outfile = str(file_path.absolute().parent) + "/" + file_path.absolute().stem + ".pdf"
+	print(outfile)
+	subprocess.Popen(["unoconvert", "--port", str(base_server_port + worker_id), infile, outfile], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
