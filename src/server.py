26f4bc3035d3422335d2d350f3e0dfb66712a0bd
Georgiy Sapronov <g.sapronov@innopolis.university>
2022-06-16 13:54:32 +0300

    Implement sending multiple files to converter

--- src/server.py
+++ src/server.py
@@ -4,6 +4,8 @@ from converter_manager import ConvertingMethod
 from pathlib import Path
 import argument_processer
 from flask import Flask, request, send_file, redirect
+import zipfile
+import os
 
 app = Flask(__name__)
 
@@ -20,14 +22,21 @@ def main():
 
 @app.route("/convert", methods=["POST"])
 def convert():
-    got = request.files['file']
-    tmp = tempfile.NamedTemporaryFile(suffix='.docx')
-    got.save(tmp)
-    args = ConversionArguments([Path(tmp.name)], None, ConvertingMethod.UNOSERVER)
-    print(tmp.name, args.input_paths, args.output_folder)
+    got = request.files.getlist('file')
+    temps = []
+    for i in range(len(got)):
+        temps.append(tempfile.NamedTemporaryFile(suffix='.docx'))
+        got[i].save(temps[-1])
+    args = ConversionArguments(list(map(Path, [q.name for q in temps])), None, ConvertingMethod.UNOSERVER)
     argument_processer.process_arguments(args)
-    filename = tmp.name[:-5] + '.pdf'
-    return send_file(filename, as_attachment=True, download_name=got.filename[:-5] + '.pdf')
+    if len(temps) == 1:
+        return send_file(temps[0].name[:-5] + '.pdf', as_attachment=True, download_name=got[0].filename[:-5] + '.pdf')
+    archive = tempfile.NamedTemporaryFile(suffix=".zip")
+    with zipfile.ZipFile(archive.name, 'w') as arch:
+        for i in range(len(got)):
+            arch.write(temps[i].name[:-5] + '.pdf', os.path.basename(got[i].filename[:-5] + '.pdf'))
+    return send_file(archive.name, as_attachment=True, download_name='documents.zip')
+    #TODO: remove pdf files after sending them
 
 
 if __name__ == "__main__":
