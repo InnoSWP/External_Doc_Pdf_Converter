1d86b0eae20f8782024d7a3a83612870bf5082ea
evsey9 <evseyru1@mail.ru>
2022-06-21 01:59:35 +0300

    convert all tabs to spaces and do some other minor reformatting

--- src/cli_convert.py
+++ src/cli_convert.py
@@ -3,9 +3,9 @@ import cli_parser
 
 
 def console_main():
-	args = cli_parser.parse_console_arguments()
-	argument_processer.process_arguments(args)
+    args = cli_parser.parse_console_arguments()
+    argument_processer.process_arguments(args)
 
 
 if __name__ == "__main__":
-	console_main()
+    console_main()
