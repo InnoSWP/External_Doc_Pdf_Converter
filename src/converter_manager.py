5681001999da6d0a3cb38278019aecf7d3bfec2f
Georgiy Sapronov <g.sapronov@innopolis.university>
2022-06-25 19:14:09 +0300

    Temporarily silence job tracking to stop polluting output

--- src/converter_manager.py
+++ src/converter_manager.py
@@ -32,7 +32,7 @@ def track_job(job, update_interval=0.1):
     global worker_total
     while job._number_left > 0:
         worker_progress = worker_total - job._number_left * job._chunksize
-        print_cur_progress()
+        #print_cur_progress()
         time.sleep(update_interval)
 
 
@@ -43,7 +43,7 @@ def print_cur_progress():
 def complete(result):
     global worker_progress
     worker_progress = worker_total
-    print_cur_progress()
+    #print_cur_progress()
 
 
 def start_conversion(args: arguments.ConversionArguments):
