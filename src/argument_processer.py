f16f4832bf5a0d97eeee8161d7047991df477b65
evsey9 <evseyru1@mail.ru>
2022-06-25 17:08:55 +0300

    fix minor issue with counting workers created

--- src/argument_processer.py
+++ src/argument_processer.py
@@ -9,4 +9,5 @@ def process_arguments(args: arguments.ConversionArguments):
     supported_formats = [".docx", ".doc", ".xls", ".xlsx"]
     args.input_paths = list(filter(lambda x: x.suffix in supported_formats, args.input_paths))
     args.input_paths.sort(key=lambda x: x.stat().st_size)  # Sort them according to filesize
+    args.proc_count = min(args.proc_count, len(args.input_paths))  # Don't make more workers than files to convert
     converter_manager.start_conversion(args)
