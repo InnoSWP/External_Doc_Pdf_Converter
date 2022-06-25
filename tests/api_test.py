a00845adfe149b334f2e3cae39f016d960d57d5a
Georgiy Sapronov <g.sapronov@innopolis.university>
2022-06-25 18:06:44 +0300

    Fix warnings in tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -5,22 +5,32 @@ server = "http://127.0.0.1:5000"
 
 class TestApi(unittest.TestCase):
     def test_corrupted(self):
-        files = {"file": open("tests/bad.docx", "rb")}
+        tmp = open("tests/bad.docx")
+        files = {"file": tmp}
         response = requests.post(server + "/convert", files=files)
         self.assertEqual(response.status_code, 400)
+        tmp.close()
 
     def test_wrong_extension(self):
-        files = {"file": open("tests/wrong.bin", "rb")}
+        tmp = open("tests/wrong.bin", "rb")
+        files = {"file": tmp}
         response = requests.post(server + "/convert", files=files)
         self.assertEqual(response.status_code, 400)
+        tmp.close()
 
     def test_single_file(self):
-        files = {"file": open("tests/good0.docx", "rb")}
+        tmp = open("tests/good0.docx", "rb")
+        files = {"file": tmp}
         response = requests.post(server + "/convert", files=files)
         self.assertEqual(response.status_code, 200)
+        tmp.close()
 
     def test_multiple_files(self):
-        files = [("file", open("tests/good0.docx", "rb")), ("file", open("tests/good1.docx", "rb"))]
+        tmp1 = open("tests/good0.docx", "rb")
+        tmp2 = open("tests/good1.docx", "rb")
+        files = [("file", tmp1), ("file", tmp2)]
         response = requests.post(server + "/convert", files=files)
         self.assertEqual(response.status_code, 200)
+        tmp1.close()
+        tmp2.close()
 
