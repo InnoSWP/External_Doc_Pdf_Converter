07b5c4dc53e8c19938e64ab5d5d7a02e7a36853e
Georgiy Sapronov <g.sapronov@innopolis.university>
2022-06-25 12:53:13 +0300

    Refactor the code of convert()

--- src/server.py
+++ src/server.py
@@ -19,36 +19,39 @@ def get_static_resource(path):
 def main():
     return redirect('/index.html')
 
-import pdb
 
 @app.route("/convert", methods=["POST"])
 def convert():
     td = tempfile.TemporaryDirectory()
-    tmp_dir = td.name
+    tmp_dir = td.name + "/"
+    original_names = []
+    converted_names = []
     got = request.files.getlist('file')
     if len(got) == 1 and got[0].filename == "" or len(got) == 0:
         abort(make_response("No files were sent to the server", 400))
     for i in range(len(got)):
         if not got[i].filename.endswith(".docx"):
             abort(make_response("File with the wrong extension was sent to the server: %s" % got[i].filename, 400))
+        original_names.append(got[i].filename)
+        converted_names.append(got[i].filename[:-5] + ".pdf")
     for i in range(len(got)):
-        got[i].save(tmp_dir + "/" + got[i].filename)
-    args = ConversionArguments(list(map(Path, [tmp_dir + "/" + q.filename for q in got])), None, ConvertingMethod.UNOSERVER)
+        got[i].save(tmp_dir + original_names[i])
+    args = ConversionArguments(list(map(Path, [tmp_dir + q for q in original_names])), None, ConvertingMethod.UNOSERVER)
     argument_processer.process_arguments(args)
-    result = None
     if len(got) == 1:
-        if os.path.exists(tmp_dir + "/" + got[0].filename[:-5] + '.pdf'):
-            base = tmp_dir + "/" + got[0].filename[:-5]
-            return send_file(base + '.pdf', as_attachment=True, download_name=got[0].filename[:-5] + '.pdf')
+        f = tmp_dir + converted_names[i]
+        if os.path.exists(f):
+            return send_file(f, as_attachment=True, download_name=converted_names[0])
         else:
             abort(make_response("The file you sent was corrupt", 400))
     for i in range(len(got)):
-        if not os.path.exists(tmp_dir + '/' + got[0].filename[:-5] + '.pdf'):
-            abort(make_response("Corrupted file was sent to the server: %s" % got[i], 400))
-    archive = tmp_dir + "/documents.zip"
+        f = tmp_dir + converted_names[i]
+        if not os.path.exists(f):
+            abort(make_response("Corrupted file was sent to the server: " + f, 400))
+    archive = tmp_dir + "documents.zip"
     with zipfile.ZipFile(archive, 'w') as arch:
         for i in range(len(got)):
-            arch.write(tmp_dir + '/' + got[i].filename[:-5] + '.pdf', got[i].filename[:-5] + '.pdf')
+            arch.write(tmp_dir + converted_names[i], converted_names[i])
     return send_file(archive, as_attachment=True)
 
 
